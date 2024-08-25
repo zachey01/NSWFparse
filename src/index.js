1ea8452f6c8f3450b7ff2b1bdf00d6f298d79b1f
zachey <63107653+zachey01@users.noreply.github.com>
2024-08-25 23:36:22 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 const { fetchRule34Posts } = require("./handlers/r34handler");
 const { fetchRedditPost } = require("./handlers/reddithandler");
 
-const furryTag = require("./tags/furry");
-const hentaiTag = require("./tags/hentai");
-const realTag = require("./tags/real");
-
 const handlers = {
   rule34: fetchRule34Posts,
-  reddit: {
-    furry: furryTag,
-    hentai: hentaiTag,
-    real: realTag,
-  },
   redditCustom: fetchRedditPost,
 };
 
